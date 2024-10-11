7b56a0e2bc0c632bc30358e967870e9289bafe1a
jayGatsby988 <darshanren@gmail.com>
2024-10-10 17:08:26 -0700

    Update AboutModal.tsx

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -54,14 +54,8 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
                   </Dialog.Title>
                   <div className="mt-2">
                     <p className="text-sm text-gray-500">
-                      This is an open source clone of the game Wordle -{" "}
-                      <a
-                        href="https://github.com/hannahcode/wordle"
-                        className="underline font-bold"
-                      >
-                        check out the code here
-                      </a>{" "}
-                      and{" "}
+                      
+                      
                       <a
                         href="https://www.powerlanguage.co.uk/wordle/"
                         className="underline font-bold"
